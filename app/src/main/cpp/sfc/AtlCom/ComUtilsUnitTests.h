5cde84daed625fb9e23c661ba405c144de32dd1c
YuujiKamura <yuujikamura@gmail.com>
2021-04-05 12:52:08 +0900

    sxf library include.

---
+++ app/src/main/cpp/sfc/AtlCom/ComUtilsUnitTests.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace AppSecInc
+{
+	namespace UnitTests 
+	{
+		namespace AtlCom
+		{
+			class ComUtilsUnitTest :  public CPPUNIT_NS::TestFixture
+			{
+				CPPUNIT_TEST_SUITE(ComUtilsUnitTest);
+				CPPUNIT_TEST(testCLSIDFrom);
+				CPPUNIT_TEST(testIsValidGUID);
+				CPPUNIT_TEST_SUITE_END();
+			public:
+				void testCLSIDFrom();
+				void testIsValidGUID();
+			};
+		}
+	}
+}
