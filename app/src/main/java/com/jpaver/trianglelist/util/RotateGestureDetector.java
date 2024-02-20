2be47b62bd8e385bd3b227322a62faacaf912e9f
YuujiKamura <yuujikamura@gmail.com>
2024-02-20 19:37:26 +0900

    RotateGestureDetector add comments

--- app/src/main/java/com/jpaver/trianglelist/util/RotateGestureDetector.java
+++ app/src/main/java/com/jpaver/trianglelist/util/RotateGestureDetector.java
@@ -1,21 +1,15 @@
 package com.jpaver.trianglelist.util;
 
-import android.content.Context;
 import android.view.MotionEvent;
 
 public class RotateGestureDetector {
     public interface OnRotateListener {
         boolean onRotate(float degrees, float focusX, float focusY);
-        boolean onRotateEnd();
     }
 
     public static class SimpleOnRotateGestureDetector implements OnRotateListener {
         @Override
         public boolean onRotate(float degrees, float focusX, float focusY) {
-            return false;
-        }
-        @Override
-        public boolean onRotateEnd() {
             return false;
         }
     }
@@ -31,25 +25,36 @@ public class RotateGestureDetector {
     }
 
     public boolean onTouchEvent(MotionEvent event) {
+        // 2本の指がタッチパネルに触れているかどうかを確認
         if (event.getPointerCount() == 2 && event.getActionMasked() == MotionEvent.ACTION_MOVE) {
             boolean result = true;
+            // 2本の指のX座標の差を計算
             float x = event.getX(1) - event.getX(0);
+            // 2本の指のY座標の差を計算
             float y = event.getY(1) - event.getY(0);
+            // 2本の指の中心点のX座標を計算
             float focusX = (event.getX(1) + event.getX(0)) * 0.5f;
+            // 2本の指の中心点のY座標を計算
             float focusY = (event.getY(1) + event.getY(0)) * 0.5f;
+            // 2本の指が形成する線とX軸との間の角度を計算（アークタンジェントを使用）
             float tan = (float) Math.atan2(y, x);
 
+            // 前回のタッチイベントからの回転があるかどうかを確認
             if (prevX != 0.0f && prevY != 0.0f) {
+                // 前回の角度からの差分を計算し、リスナーを通じて回転イベントを処理
                 result = listener.onRotate((tan - prevTan) * RADIAN_TO_DEGREES, focusX, focusY);
             }
 
+            // 現在の値を「前回の値」として保存
             prevX = x;
             prevY = y;
             prevTan = tan;
             return result;
         } else {
+            // 2本の指のタッチがない場合は、前回の値をリセット
             prevX = prevY = prevTan = 0.0f;
             return true;
         }
     }
+
 }
