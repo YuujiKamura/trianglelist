5ee39053c9727f040b2cbde458ab5a85c675ec94
yuuji <yuujikamura@gmail.com>
2024-04-22 21:13:04 +0900

    getenv CI test..

--- app/src/test/java/com/jpaver/trianglelist/SfcWriterTest.kt
+++ app/src/test/java/com/jpaver/trianglelist/SfcWriterTest.kt
@@ -1,9 +1,7 @@
 package com.jpaver.trianglelist
 
 import com.jpaver.trianglelist.util.FileUtil
-import org.junit.After
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 import java.io.File
 
@@ -21,25 +19,15 @@ class SfcWriterTest {
         if (System.getenv("CI") != null) return
         // CI環境ではこのテストをスキップ
 
+        // テスト用のディレクトリとファイルが既に存在する場合は削除
+        testFile.parentFile?.deleteRecursively()
+
         // テスト内容をユーザーのホームディレクトリに書き出し
         FileUtil.writeToUserHome(testContent, testPath)
 
         // ファイルが正しく作成され、内容が期待通りか確認
         Assert.assertTrue("File was not created.", testFile.exists())
         Assert.assertTrue("Content does not match.", testFile.readText() == testContent)
-    }
-
-    @Before
-    fun setUp() {
-        // テスト用のディレクトリとファイルが既に存在する場合は削除
-        testFile.parentFile?.deleteRecursively()
-    }
-
-    @After
-    fun tearDown() {
-        // テストで作成したファイルとディレクトリを削除してクリーンアップ
-        //testFile.delete()
-        //testDirectory.delete() // ディレクトリが空であることを確認してから削除
     }
 
 }
